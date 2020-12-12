d99976e25cc53586455ba137abe0bc6e0eacb8cf
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-13 03:02:47 +0900

    stream.Readableを継承した自前の読み込みストリームを実装

---
+++ chapter3/HelloReadableStream.js
@@ -0,0 +1,36 @@
+const stream = require('stream')
+
+class HelloReadableStream extends stream.Readable {
+  constructor(options, languages) {
+    super(options)
+    this.languages = languages
+  }
+
+  _read() {
+    let language
+
+    while ((language = this.languages.shift())) {
+      if (!this.push(language)) {
+        console.log('読み込み中断')
+        return
+      }
+    }
+    console.log('読み込み完了')
+    this.push(null)
+  }
+}
+
+const helloReadableStream = new HelloReadableStream(null, [
+  'JavaScript',
+  'TypeScript',
+  'Ruby',
+])
+helloReadableStream
+  .on('readable', () => {
+    console.log('readable')
+    let chunk
+    while ((chunk = helloReadableStream.read()) !== null) {
+      console.log(chunk.toString())
+    }
+  })
+  .on('end', () => console.log('end'))
