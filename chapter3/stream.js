9c793d61443362325b670a013f7c23340e21776d
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-12 22:24:02 +0900

    pipeで機能を結合できる、を確認

--- chapter3/stream.js
+++ chapter3/stream.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
+const crypto = require('crypto')
 
 const copyFileWithStream = (src, dest, callback) => {
   fs.createReadStream(src)
+    .pipe(crypto.createHash('sha256'))
     .pipe(fs.createWriteStream(dest))
     .on('finish', () => callback('finish'))
 }
