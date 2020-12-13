03c817ffaef01db76ec10f7ca50628dc1e478428
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-13 18:26:20 +0900

    pipeでストリームを連結

--- chapter3/pipe.js
+++ chapter3/pipe.js
@@ -2,3 +2,7 @@ const HelloReadableStream = require('./HelloReadableStream')
 const DelayLogStream = require('./DelayLogStream')
 const LineTransformStream = require('./LineTransformStream')
 
+new HelloReadableStream(null, ['Ruby', 'TypeScript'])
+  .pipe(new LineTransformStream())
+  .pipe(new DelayLogStream())
+  .on('finish', () => console.log('完了'))
